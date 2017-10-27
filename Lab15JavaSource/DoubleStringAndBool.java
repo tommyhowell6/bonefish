e410ee5b91c6155d22ebfdb6387d61312138ba53
jesser1212 <jesser1212@gmail.com>
2017-10-27 12:34:50 -0600

    Adding working Java Rosalind15 lab
    This is a functioning Java implementation of Rosalind 15's paired read reconstruction. It might be helpful to look over if you weren't able to complete pair reading as we try to implement a Python version and add error handling.

---
+++ Lab15JavaSource/DoubleStringAndBool.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Jesse on 10/17/2017.
+ */
+public class DoubleStringAndBool {
+    public DoubleString edgeDestination;
+    public boolean used;
+
+    public DoubleStringAndBool(DoubleString edgeDestination) {
+        this.edgeDestination = edgeDestination;
+        used = false;
+    }
+
+}
