cd8a13634002107b6f243473d74a5b1432b35a0a
Tommy Howell <tommyhowell6@gmail.com>
2017-12-04 16:27:11 -0700

    added Trie code

---
+++ bonefishGenomeSequence/src/TrieSquencer/GenomeSequencer.java
@@ -0,0 +1,117 @@
+package com.bonefishtrie;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by tommyhowell on 11/14/17.
+ */
+
+public class GenomeSequencer
+{
+    Node root;
+    private char[] intToCharConverter = "ACGT".toCharArray();
+    public GenomeSequencer()
+    {
+        //R is for root. This should not ever get read
+        root = new Node('R');
+    }
+
+
+    public void addNode(String s)
+    {
+        root.add(s.substring(1));
+    }
+
+    //input a string and it will build the longest genome that it can.
+    //If it can't find a path on the string passed in it will return without any genome.
+    public String runStartAgainstTrie(String start)
+    {
+        StringBuilder searchString = new StringBuilder(start.substring(2));
+        StringBuilder toReturn = new StringBuilder(start);
+        Node node = getChildOfPath(searchString.toString());
+        if(node == null)
+        {
+            //only in here when the string that we thought was a start does not have a path in the trie.
+            //only errors reads will be here
+            return "likely error";
+        }
+        node = node.getOnlyChild();
+        toReturn.append(node.getId());
+        boolean working = true;
+        while (working)
+        {
+            searchString.delete(0,1);
+            searchString.append(node.getId());
+            node = getChildOfPath(searchString.toString());
+            if(node == null)
+            {
+                System.out.println(toReturn.length());
+                return toReturn.toString();
+            }
+            else
+            {
+                node = node.getOnlyChild();
+            }
+            if(node.getNumberOfChildren() > 1)
+            {
+                //If we do find ourselves in here we need to add error checking as the next step.
+                //I would say take the high quality read. if they are the same... maybe some type if banch.
+                System.out.println("In runStartAgainstTrie ERROR multiple children found " + node.getNumberOfChildren());
+                working = false;
+            }
+            else
+            {
+                toReturn.append(node.getId());
+            }
+        }
+        System.out.println("returning");
+        return toReturn.toString();
+    }
+
+
+
+    //this method returns a node that you find from following the string passed in.
+    @Nullable
+    private Node getChildOfPath(@NonNull String s)
+    {
+        Node node = root;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if(node != null)
+            {
+                node = node.getChild(s.charAt(i));
+            }
+        }
+        return node;
+    }
+
+    //returns true if the prefix of the string passed in is not in the trie
+    //used to find a possible start.
+    //note error reads will also return true.
+    public boolean prefixNotInTrie(String s)
+    {
+        String pre = s.substring(0,s.length() - 1);
+        Node node = root;
+        for (int i = 0; i < pre.length(); i++)
+        {
+            if(node == null)
+            {
+                return true;
+            }
+            else if(node.getChildren() == null)
+            {
+                return true;
+            }
+            else if(node.getChild(s.charAt(i)) == null)
+            {
+                return true;
+            }
+            else
+            {
+                node = node.getChild(s.charAt(i));
+            }
+        }
+        return false;
+    }
+}
---
+++ bonefishGenomeSequence/src/TrieSquencer/Node.java
@@ -0,0 +1,213 @@
+package com.bonefishtrie;
+
+import android.widget.Switch;
+
+/**
+ * Created by tommyhowell on 11/14/17.
+ */
+
+public class Node
+{
+    private Node [] children;
+    private int numberOfChildren;
+    private int count;
+    private char id;
+
+    public Node(char id)
+    {
+        this.id = id;
+        numberOfChildren = 0;
+    }
+
+    public boolean deletePath(String s)
+    {
+        if(s.length() == 1)
+        {
+            if(children == null)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            switch (s.toUpperCase().charAt(0))
+            {
+                case 'A':
+                    if (children[0] != null)
+                    {
+                        if (deletePath(s.substring(1)))
+                        {
+
+                        }
+                    }
+                    break;
+                case 'C':
+
+                    break;
+                case 'G':
+                    break;
+                case 'T':
+                    break;
+            }
+        }
+        return false;
+    }
+
+    public void add(String s)
+    {
+        if(children == null)
+        {
+            children = new Node[4];
+        }
+        Node temp;
+        switch (s.toUpperCase().charAt(0))
+        {
+            case 'A':
+                if(children[0] == null)
+                {
+                    temp = new Node('A');
+                    numberOfChildren++;
+                }
+                else
+                {
+                    temp = children[0];
+                }
+                if(s.length() > 1)
+                {
+                    temp.add(s.substring(1));
+                }
+                else
+                {
+                    count++;
+                }
+                children[0] = temp;
+                break;
+            case 'C':
+                if(children[1] == null)
+                {
+                    temp = new Node('C');
+                    numberOfChildren++;
+                }
+                else
+                {
+                    temp = children[1];
+                }
+                if(s.length() > 1)
+                {
+                    temp.add(s.substring(1));
+                }
+                else
+                {
+                    count++;
+                }
+                children[1] = temp;
+                break;
+            case 'G':
+                if(children[2] == null)
+                {
+                    temp = new Node('G');
+                    numberOfChildren++;
+                }
+                else
+                {
+                    temp = children[2];
+                }
+                if(s.length() > 1)
+                {
+                    temp.add(s.substring(1));
+                }
+                else
+                {
+                    count++;
+                }
+                children[2] = temp;
+                break;
+            case 'T':
+                if(children[3] == null)
+                {
+                    temp = new Node('T');
+                    numberOfChildren++;
+                }
+                else
+                {
+                    temp = children[3];
+                }
+                if(s.length() > 1)
+                {
+                    temp.add(s.substring(1));
+                }
+                else
+                {
+                    count++;
+                }
+                children[3] = temp;
+                break;
+        }
+
+    }
+
+    public int getNumberOfChildren()
+    {
+        return numberOfChildren;
+    }
+
+    public Node getChild(char c)
+    {
+        switch (c)
+        {
+            case 'A':
+                if(children[0] != null)
+                {
+                    return children[0];
+                }
+                break;
+            case 'C':
+                if(children[1] != null)
+                {
+                    return children[1];
+                }
+                break;
+            case 'G':
+                if(children[2] != null)
+                {
+                    return children[2];
+                }
+                break;
+            case 'T':
+                if(children[3] != null)
+                {
+                    return children[3];
+                }
+                break;
+        }
+        return null;
+    }
+
+    public char getId()
+    {
+        return id;
+    }
+
+    public Node[] getChildren()
+    {
+        return children;
+    }
+
+
+    public Node getOnlyChild()
+    {
+            if(children == null)
+            {
+                return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if(children[i] != null)
+                {
+                    return children[i];
+                }
+            }
+            return null;
+    }
+
+}
